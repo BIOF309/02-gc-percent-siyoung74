a10625fad0b7028340e9bfc000961332999a9b0a
siyoung74 <31741329+siyoung74@users.noreply.github.com>
2017-10-23 18:28:03 -0400

    Update 02-gc-percent-siyoung74.py

--- 02-gc-percent-siyoung74.py
+++ 02-gc-percent-siyoung74.py
@@ -1,7 +1,14 @@
 # 02-gc-percent-siyoung74
-seq=input("input your seq ")
+
+# This sequence is the first 100 nucleotides of the Influenza H1N1 Virus segment 8
+#flu_ns1_seq = 'GTGACAAAGACATAATGGATCCAAACACTGTGTCAAGCTTTCAGGTAGATTGCTTTCTTTGGCATGTCCGCAAACGAGTTGCAGACCAAGAACTAGGTGA'
+seq_raw = input("Please enter sequence into GC calculator: ")
+print("The sequence you enter is: " + seq_raw)
+seq = seq_raw.upper()
+print(seq)
+#seq=input("input your seq ")
 total=len(seq)
-g=seq.count("g")
-c=seq.count("c")
+g=seq.count("G")
+c=seq.count("C")
 percent=(g+c)/total*100
 print("GC %:", percent, "%")
